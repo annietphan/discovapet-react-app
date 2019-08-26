d37bd7feea740409fb3d1daa79ec1bb09181b389
Annie Phan <anniephan.atp@gmail.com>
2019-08-26 11:46:41 -0700

    give Provider access to store

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
