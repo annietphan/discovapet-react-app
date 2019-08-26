9106ef4bcf54576cf25823fb2951f62d91f62f11
Annie Phan <anniephan.atp@gmail.com>
2019-08-26 11:40:51 -0700

    add redux, thunk, router

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 class App extends React.Component {
 
-  componentDidMount(){
-    fetch('http://localhost:3000/api/v1/shelters')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/api/v1/shelters')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
 
   render() {
     return(
