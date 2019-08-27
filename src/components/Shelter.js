ca76429753a9b30e0cf01593e68ec1f16fe99e76
Annie Phan <anniephan.atp@gmail.com>
2019-08-27 11:30:48 -0700

    install bootstrap

--- src/components/Shelter.js
+++ src/components/Shelter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import {Redirect} from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
 import PetsContainer from '../containers/PetsContainer'
 
 const Shelter = ({match, shelters}) => {
@@ -9,18 +9,30 @@ const Shelter = ({match, shelters}) => {
   // eslint-disable-next-line
   // const shelter = props.shelters.filter(shelter => shelter.id == props.match.params.id)[0]
   const shelter = shelters[match.params.id - 1]
+  if (shelter) {
+    return(
+      <div className="shelter">
+        <h2>{shelter.name}</h2>
+      </div>
+    )
+  } else {
+    return (
+      <Redirect to='/shelters'/>
+    )
+  }
 
-  return (
-    <div>
-      {/*shelter ? null : <Redirect to='/shelters' /> */}
-      <h2>{shelter ? shelter.name : null}</h2>
-      <p>{shelter ? shelter.street_address : null}</p>
-      <p>{shelter ? shelter.city : null}, {shelter ? shelter.state : null} {shelter ? shelter.zipcode : null}</p>
-      {/*<p>Animals: {shelter ? shelter.pets.length : null}</p><br />*/}
-      <h4>Pets:</h4>
-      <PetsContainer shelter={shelter}/>
-    </div>
-  )
+  // return (
+  //   <div>
+  //     {shelter ? return shelter.name}
+  //     {/*shelter ? null : <Redirect to='/shelters' /> */}
+  //     <h2>{shelter.name}</h2>
+  //     <p>{shelter ? shelter.street_address : null}</p>
+  //     <p>{shelter ? shelter.city : null}, {shelter ? shelter.state : null} {shelter ? shelter.zipcode : null}</p>
+  //     {/*<p>Animals: {shelter ? shelter.pets.length : null}</p><br />*/}
+  //     <h4>Pets:</h4>
+  //     <PetsContainer shelter={shelter}/>
+  //   </div>
+  // )
 }
 
 export default Shelter
