12f6ae43f6b58c670a57124c8e71cf27a5c8263c
Annie Phan <anniephan.atp@gmail.com>
2019-08-26 22:18:26 -0700

    setup pet form

--- src/components/PetInput.js
+++ src/components/PetInput.js
@@ -6,10 +6,41 @@ class PetInput extends Component {
   render() {
     return(
       <div>
-      PetInput
+        <h4>Add Pet:</h4>
+        <form>
+          <label>Name:</label>
+          <input type="text" placeholder="Name" /><br />
+          <label>Pet Type:</label>
+            <select>
+              <option>dog</option>
+              <option>cat</option>
+            </select><br />
+          <label>Gender:</label>
+            <select>
+              <option>male</option>
+              <option>female</option>
+            </select><br />
+          <label>Breed:</label>
+          <input type="text" placeholder="Breed" /><br />
+          <label>Size:</label>
+          <input type="text" placeholder="Size" /><br />
+          <label>Age:</label>
+          <input type="text" placeholder="Age" /><br />
+          <label>Vaccinations up to date?</label>
+          <input type="checkbox" /><br />
+          <label>House-trained?</label>
+          <input type="checkbox"  /><br />
+          <label>Good with cats?</label>
+          <input type="checkbox"  /><br />
+          <label>Good with dogs?</label>
+          <input type="checkbox"  /><br />
+          <label>Good with kids?</label>
+          <input type="checkbox"  /><br />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
 }
 
-export default connect()(PetInput)
+export default connect(null)(PetInput)
