68d5493063adbdb9c0842432ea52d299f2dda927
Annie Phan <anniephan.atp@gmail.com>
2019-08-28 11:51:06 -0700

    add petcard styling

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -3,12 +3,12 @@ import img2 from '../assets/IMG_6415.jpg'
 
 const PetCard = props => {
   return(
-    <div className='petcard text-center'>
-      <div className='overflow'>
-        <img src={img2} alt=""  className="card-img-top"/>
-      </div>
-      <div className='petcard-body text-dark'>
+    <div className='card text-center'>
+      <div className='card-header text-dark'>
         <p>{props.pet.name}</p>
+      </div>
+      <div className='overflow'>
+        <img src={img2} alt=""  className="card-img-bottom"/>
       </div>
     </div>
   )
